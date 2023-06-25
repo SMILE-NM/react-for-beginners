25afb136e27a30f0a55141d3e3b6c91b98702681
Nematov Muhammadjon <123355875+SMILE-NM@users.noreply.github.com>
2023-06-25 22:38:17 +0500

    Users program 70%

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
+
+//INFO: This link list users in json format: https://reqres.in/api/users
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Server is not working. We haven't list users");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
+      {/* <Success /> */}
+    </div>
+  );
+}
+
+export default App;
